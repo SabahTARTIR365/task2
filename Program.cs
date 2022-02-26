41c6134ad707905619f808777eff285c8d29807b
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-02-26 16:36:11 +0200

    adding taske files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace StackTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //double sum = 0.0;
+            Console.WriteLine("Hello World!");
+            var stack = new STACK<double>();
+            stack.Push(1.5);
+            stack.Push(37.5);
+            stack.Push(5.2);
+            stack.Print();
+            stack.Pop();
+            stack.Print();
+            stack.Peak();
+            stack.Clear();
+             // System.Console.WriteLine($"sum{sum}");
+             stack.Peak();
+             
+        }
+    }
+}
